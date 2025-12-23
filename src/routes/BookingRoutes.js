6031aba81f18e54f90eec7f095d6356cdd78da60
Yazantrissi <yazanttr434@gmail.com>
2025-12-23 03:17:55 -0900

    Cleanup: Remove duplicate files from root and keep only backend folder

--- src/routes/BookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const BookingController = require('../controllers/BookingController');
-const auth = require('../middleware/auth');
-
-// حماية الروابط بالـ Token
-router.post('/group', auth, BookingController.initiateGroupBooking);
-
-module.exports = router;
